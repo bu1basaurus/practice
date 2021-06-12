9dc2f807d78e4b9a5e6c083d742b4a44decdeee4
Olga Rudkovskaya <rudkovskai95@gmail.com>
2021-06-12 13:55:54 +0300

    12.06

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -5,35 +5,22 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour
 {
     public GameObject Platform;
-
     public Transform GenerationPoint;
-
     public float DistanceBetween;
-
     float PlatformWidth;
-
-
-  
 
 
     void Start()
     {
         PlatformWidth = 27;
-
     }
-
 
     void Update()
     {
-        
          if (transform.position.x < GenerationPoint.position.x)
         {
             transform.position = new Vector3(transform.position.x + PlatformWidth, transform.position.y, transform.position.z);
-
             Instantiate(Platform, transform.position, transform.rotation);
-
         } 
-
     }
-    
 }
