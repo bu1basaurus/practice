3a7a70fbb24555b7be0e3468db013674a5929adc
Olga Rudkovskaya <rudkovskai95@gmail.com>
2021-06-16 10:26:07 +0300

    16.06_1

---
+++ Assets/Scripts/BarrierController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierController : MonoBehaviour
+{
+    public float Movespeed;
+    bool MovingUp = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.y > 2.5f)
+        {
+            MovingUp = false;
+        }
+        else if (transform.position.y < -2.5f)
+        {
+            MovingUp = true;
+        }
+
+        if (MovingUp)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + Movespeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - Movespeed * Time.deltaTime);
+        }
+    }
+}
